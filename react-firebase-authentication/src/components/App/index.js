896060dbde312d243bed58d7ba295cef1a574162
Yajaira Gracia <yajairagracia@Yajairas-MacBook-Pro.local>
2021-12-29 12:58:34 -0600

    routes

--- react-firebase-authentication/src/components/App/index.js
+++ react-firebase-authentication/src/components/App/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswrodForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
  <Router>
